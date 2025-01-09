85030ce4cbc38a6a3194ee85766c6ff715523cfd
mapi <rfriends2017@ymail.ne.jp>
2025-01-09 09:31:43 +0900

    Create t.sh

---
+++ t.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# easy install friends for termux
+#
+# 1.0 2025/01/09 new
+ver=1.0
+#
+echo
+echo easy install friends for termux Vr. $ver
+echo 
+termux-change-repo
+termux-setup-storage
+pkg update && pkg upgrade -y
+pkg install git -y
+cd ~/
+rm -rf rfriends_termux
+git clone https://github.com/rfriends/rfriends_termux.git
+cd rfriends_termux
+sh rfriends_termux.sh
+echo
+echo finished
