e4707ec7236cd6efaa6c3da4d17e52d6613285d7
mapi <rfriends2017@ymail.ne.jp>
2025-02-02 08:27:38 +0900

    Add files via upload

---
+++ termux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+#
+# easy install friends for termux
+#
+# 1.0 2025/01/09 new
+# 1.0 2025/02/02 fix
+ver=1.1
+#
+echo
+echo easy install friends for termux Vr. $ver
+echo 
+#termux-change-repo
+#termux-setup-storage
+pkg update
+pkg install git -y
+cd ~/
+rm -rf rfriends_termux
+git clone https://github.com/rfriends/rfriends_termux.git
+cd rfriends_termux
+sh rfriends_termux.sh
+echo
+echo finished
+
