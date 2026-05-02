a23ea4116d7e91eb9be0c54d1959de0f447e8c90
mapi <rfriends2017@ymail.ne.jp>
2026-05-03 03:57:57 +0900

    Update svenable.sh

--- svenable.sh
+++ svenable.sh
@@ -7,9 +7,10 @@
 # 1.11 2024/07/31 easy
 # 2.00 2024/11/03 add lighttpd
 # 2.10 2026/02/05 add samba
+# 2.11 2026/05/03 add pulseaudio
 #=======================================
 echo
-echo sv-enable 2.10
+echo sv-enable 2.11
 echo
 echo 以下の5つのコマンドを実行します。
 echo
@@ -17,6 +18,7 @@ echo sv-enable atd
 echo sv-enable crond 
 echo sv-enable sshd
 echo sv-enable lighttpd
+echo pulseaudio start
 echo smbd -D -s $PREFIX/etc/smb.conf
 echo termux-wake-lock
 echo
@@ -25,6 +27,7 @@ sv-enable atd
 sv-enable crond
 sv-enable sshd
 sv-enable lighttpd
+pulseaudio --start --exit-idle-time=-1
 smbd -D -s $PREFIX/etc/smb.conf
 # 
 termux-wake-lock
