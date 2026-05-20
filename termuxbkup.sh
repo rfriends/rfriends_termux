afa3b52f583ed4d5db62ecbdd521073c51272563
mapi <rfriends2017@ymail.ne.jp>
2026-05-21 01:42:45 +0900

    Add files via upload

---
+++ termuxbkup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+#
+# backup rfriends3 image for termux
+#
+# 1.0 2025/05/21 new
+ver=1.0
+#
+archi=$(dpkg --print-architecture)
+echo
+echo backup rfriends3 image for termux( $archi ) Ver. $ver
+echo 
+
+if [ "$archi" = 'arm' ]; then
+  echo 'arm architecture'
+  cpu=v7a
+elif [ "$archi" = 'aarch64' ]; then
+  echo 'aarch64 architecture'
+  cpu=v8a
+else
+  echo "$archi unknown architecture"
+  exit 1
+fi
+
+dir=/sdcard/Download
+target=/data/data/com.termux/files
+dat=20260520
+fil=termux-backup-$cpu-$dat.tar.gz
+
+cd ~/
+echo "tar -zcvf $dir/$fil -C $terget ./usr ./home > /dev/null"
+#tar -zcvf $dir/$fil -C $terget ./usr ./home > /dev/null
+
+echo
+echo finished
+echo
+exit 0
---
+++ termuxrst.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+#
+# restore rfriends3 image for termux
+#
+# 1.0 2025/05/20 new
+# 1.1 2025/05/21 fix
+ver=1.1
+#
+archi=$(dpkg --print-architecture)
+echo
+echo restore rfriends3 image for termux( $archi ) Ver. $ver
+echo 
+
+if [ "$archi" = 'arm' ]; then
+  echo 'arm architecture'
+  cpu=v7a
+elif [ "$archi" = 'aarch64' ]; then
+  echo 'aarch64 architecture'
+  cpu=v8a
+else
+  echo "$archi unknown architecture"
+  exit 1
+fi
+
+site=https://ss1.xrea.com/rfbuddy.s296.xrea.com/storage
+terget=/data/data/com.termux/files
+dir=/sdcard/Download
+dat=20260520
+fil=termux-backup-$cpu-$dat.tar.gz
+
+cd ~/
+pkg install -y wget
+echo "wget $site/$fil -O $dir/$fil"
+#wget $site/$fil -O $dir/$fil
+
+echo "tar -zxvf $dir/$fil -C $terget --recursive-unlink --preserve-permissions > /dev/null"
+#tar -zxvf $dir/$fil -C $terget --recursive-unlink --preserve-permissions > /dev/null
+
+echo
+echo finished
+echo
+exit 0
